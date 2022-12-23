70bab80f470ff7272e81f6671e5b789b8bf6c8d4
argen1234 <106479453+argen1234@users.noreply.github.com>
2022-12-23 22:39:45 +0100

    update header
    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-    <nav className="bg-white d-flex px-5 py-2">
-    <h1 className='fw-bold'>My trips <span className='fw-light'>diary</span></h1>
-   </nav> 
-</div>
-  )
+    <div className="container-fluid d-flex px-5 py-2 justify-content-between bg-white border border-b  ">
+    <div className="d-flex container w-75 ">
+        <h1 className='fw-bold'>My trips <span className="fw-light">diary</span></h1> 
+    </div>
+        <div className="ms-5 my-auto me-auto align-self-end">
+    </div> 
+  </div>
+    )
 }
 
 export default Header;
