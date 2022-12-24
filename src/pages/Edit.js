7afd92fbc2b811bf5a0a226c3d087d37d7d9ac09
Jurgen <Jurgen.Xibrraku@fti.edu.al>
2022-12-24 10:11:49 +0100

    further updating

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -30,7 +30,12 @@ function Edit() {
         }
         else {
           setErrorEmail(data);
-        } 
+        }
+        
+      setTimeout (() => {
+        setErrorEmail('');
+      }, 3000);   
+      form.reset();
     }
       
     async function updatePassword(e) {
@@ -41,7 +46,7 @@ function Edit() {
       const password = formData.get("newPassword");
       const value = {password};
         
-      if ((currentPassword === passwordLogedin) && (password != currentPassword)) {
+      if ((currentPassword === passwordLogedin) && (password !== currentPassword)) {
         const response = await fetch(`http://localhost:4000/api/password/${id}`, {
           method:'PUT', headers: {
             'Content-Type': 'application/json'
@@ -63,6 +68,11 @@ function Edit() {
         else {
             setErrorPassword(`This is not your current password. Please try again!`);
            }
+
+      setTimeout (() => {
+        setErrorPassword('');
+      }, 3000);
+      form.reset();
     }
 
   return (
@@ -73,7 +83,7 @@ function Edit() {
         <h2 className='fw-bold'>Personal information</h2>
         <form onSubmit={updateEmail} className='d-flex flex-column w-100'>
           <label className=" my-2 fw-bold fs-6">Email</label>
-          <input className="p-1 w-50" type='text' name='email' placeholder='john.doe@gmail.com' />
+          <input className="p-1 w-50" type='email' name='email' placeholder='john.doe@gmail.com' />
           <div className='d-flex justify-content-end my-2'>
             <button type='submit' className='btn btn-secondary px-5'>Save</button>
           </div>
@@ -100,7 +110,7 @@ function Edit() {
       </div>
       <div className='text-center text-danger w-75 mx-auto bg-white'>
         {(errorEmail) ? <p>{errorEmail}</p> : null }
-        {(errorPassword) ?<p>{errorPassword}</p> : null}
+        {(errorPassword) ? <p>{errorPassword}</p> : null }
       </div>
       <Footer />
     </div>
